cad9f035f48b9d2b6bff4c9d07c393fc9559fca1
geekedout1013 <reeser1013@gmail.com>
2023-05-06 16:58:04 -0500

    5/4/23 changes

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        
+       Console.WriteLine("What is your grade?");
+        string grade = Console.ReadLine();
+        int number = int.Parse(grade);
+
+        if (number < 90)
+        {
+            if (number < 90 && number > 79)
+
+            {
+                Console.WriteLine("You have a B in this clas.");
+            }
+            else if (number < 80 && number > 69)
+            {
+                Console.WriteLine("You have a C in this class.");
+            }
+            else if (number < 70 && number > 59)
+            {
+                Console.WriteLine("You have a D in this class.");
+            }
+            else
+            {
+                Console.WriteLine("You have an F in this class.");
+
+            }
+        }
+        else
+        {
+            Console.WriteLine("You have an A in this class.");
+        }
     }
 }
